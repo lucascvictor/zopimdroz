dda08c6af43ed327be8bd15b7d97ad769794cddd
lukevictor <lucas.c.victor@hotmail.com>
2018-06-14 08:45:50 -0300

    Separação de funcoes para remocao e criacao de chats independetentes

--- iZopim.js
+++ iZopim.js
@@ -1,6 +1,12 @@
     window.setInterval('zopim()', 2000);
+    window.setInterval('exibe()', 2000);
+
      function zopim() {
          $zopim(function() {
                  $zopim.livechat.departments.setVisitorDepartment('Fly01 Manufatura');
              });
+     }
+
+     function exibe() {
+        console.log(prechat_inputs);
      }
--- initChat.js
+++ initChat.js
@@ -1,43 +1,45 @@
+var prechat_inputs = [];
+prechat_inputs['name'] = 'Lucas';
 
 function manufatura() {
-  
-
-        var prechat_inputs = []; 
-        prechat_inputs['name'] = 'Nome do Cliente'; 
-
-    (function(d,r,o,z,a,k,t){
-          a=d.getElementsByTagName('head')[0];
-          k=d.createElement('script');k.async=1;
-          t=(Math.floor(Math.random()*1000000)+1);
-          k.src=r+'/'+o+'/droz.js?i='+z+'&u='+r+'&v='+t;
-          a.appendChild(k);
-        })(document,'https://chat-app.meudroz.com','v1','63dee53e15d58dfce07eaaa496a479bac1376d31');
+    var key = '63dee53e15d58dfce07eaaa496a479bac1376d31';
+    reinit();
+    createChat(key);
+    initChat();
   }
 
 function varejo() {
-    $('#drz_btn_open').remove();
-    $('#droz-div').remove();
-    (function(d,r,o,z,a,k,t){
-          a=d.getElementsByTagName('head')[0];
-          k=d.createElement('script');k.async=1;
-          t=(Math.floor(Math.random()*1000000)+1);
-          k.src=r+'/'+o+'/droz.js?i='+z+'&u='+r+'&v='+t;
-          a.appendChild(k);
-        })(document,'https://chat-app.meudroz.com','v1','be8b72062018b69b54062314aff176cbe10da270');
+    var key = 'be8b72062018b69b54062314aff176cbe10da270';
+    reinit();
+    createChat(key);
+    initChat();
   }
 
 function bemacash() {
-    $('#drz_btn_open').remove();
-    $('#droz-div').remove();
-    (function(d,r,o,z,a,k,t){
-          a=d.getElementsByTagName('head')[0];
-          k=d.createElement('script');k.async=1;
-          t=(Math.floor(Math.random()*1000000)+1);
-          k.src=r+'/'+o+'/droz.js?i='+z+'&u='+r+'&v='+t;
-          a.appendChild(k);
-        })(document,'https://chat-app.meudroz.com','v1','3e9ee6c5c4656822f19621bfbf8416972400751b');
+    var key = '3e9ee6c5c4656822f19621bfbf8416972400751b';
+    reinit(); 
+    createChat(key);
+    initChat();
   }
 
 function initChat() {
     $('#drz_btn_open').click();
+}
+
+function reinit() {
+    $('header').remove();
+    $('#drz_btn_open').remove();
+    $('#droz-div').remove();
+    var hdr = document.createElement("header");                        
+    document.body.appendChild(hdr); 
+}
+
+function createChat(key) {
+  (function(d,r,o,z,a,k,t){
+    a=d.getElementsByTagName('header')[0];
+    k=d.createElement('script');k.async=1;
+    t=(Math.floor(Math.random()*1000000)+1);
+    k.src=r+'/'+o+'/droz.js?i='+z+'&u='+r+'&v='+t;
+    a.appendChild(k);
+  })(document,'https://chat-app.meudroz.com','v1', key);
 }
