70c291e146e51abbb8207b1f7e7eb0afafa8bebc
lukevictor <lucas.c.victor@hotmail.com>
2018-06-14 11:53:01 -0300

    Correção da inicilização por departamento

--- iZopim.js
+++ iZopim.js
@@ -1,9 +1,20 @@
-    function intervalar(dep) {
-        window.setInterval('zopim(dep)', 2000);
-    }
-    
-     function zopim(departamento) {
-         $zopim(function() {
-                 $zopim.livechat.departments.setVisitorDepartment(departamento);
-             });
-     }
+var dep = "";
+
+function setDep(depart) {
+  dep = depart;
+}
+
+function getDep() {
+  return dep;
+}
+
+function intervalar() {
+    window.setInterval('zopim()', 2000);
+  }
+  
+  function zopim() {
+     $zopim(function() {
+             $zopim.livechat.departments.setVisitorDepartment(getDep());
+         });
+  }
+  
--- initChat.js
+++ initChat.js
@@ -1,26 +1,29 @@
 
 function manufatura() {
     var key = '63dee53e15d58dfce07eaaa496a479bac1376d31';
+    setDep('Fly01 Manufatura');
     reinit();
     createChat(key);
     initChat();
-    intervalar('Fly01 Manufatura');
+    intervalar(dep);
   }
 
 function varejo() {
     var key = 'be8b72062018b69b54062314aff176cbe10da270';
+    setDep('Fly01 Varejo');
     reinit();
     createChat(key);
     initChat();
-    intervalar('Fly01 Varejo');
+    intervalar(dep);
   }
 
 function bemacash() {
     var key = '3e9ee6c5c4656822f19621bfbf8416972400751b';
+    setDep('Bemacash');
     reinit(); 
     createChat(key);
     initChat();
-    intervalar('Bemacash');
+    intervalar(dep);
   }
 
 function initChat() {
@@ -44,3 +47,5 @@ function createChat(key) {
     a.appendChild(k);
   })(document,'https://chat-app.meudroz.com','v1', key);
 }
+
+
