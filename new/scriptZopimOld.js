6977133a9ce943228152687af677afd49812f630
lukevictor <lucas.c.victor@hotmail.com>
2018-08-22 17:34:18 -0300

    Correções para a chamada do Zopim

--- new/scriptZopimOld.js
+++ new/scriptZopimOld.js
@@ -1,3 +1,4 @@
+<!-- Scripts antigos que estavam no painel do droz para chamadas do Zopim-->
 <!-- BEMACASH-->
 <!-- Start of totvssuporte Zendesk Widget script -->
 <script>/*<![CDATA[*/window.zE||(function(e,t,s){var n=window.zE=window.zEmbed=function(){n._.push(arguments)}, a=n.s=e.createElement(t),r=e.getElementsByTagName(t)[0];n.set=function(e){ n.set._.push(e)},n._=[],n.set._=[],a.async=true,a.setAttribute("charset","utf-8"), a.src="https://static.zdassets.com/ekr/asset_composer.js?key="+s, n.t=+new Date,a.type="text/javascript",r.parentNode.insertBefore(a,r)})(document,"script","9c45e410-744f-4aae-be0f-e7038becfe17");window.zESettings = { webWidget: { position: { horizontal: "left", vertical: "bottom" }, contactForm: { tags: ["chat_bemacash"] } } }; !!zE && zE(() => {$zopim(() => {$zopim.livechat.set({language: "pt-BR",});$zopim.livechat.departments.filter("Bemacash");$zopim.livechat.departments.setVisitorDepartment("Bemacash");});zE.show();});/*]]>*/</script>
