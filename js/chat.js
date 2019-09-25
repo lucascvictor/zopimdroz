92d1db523ef7dfbf00d92b15a3dadd9de785cde6
Lucas da Cunha Victor <lucas.victor@totvs.com.br>
2019-09-25 17:36:33 -0300

    Correções para redirecionamento de páginas

--- js/chat.js
+++ js/chat.js
@@ -1,3 +1,4 @@
+
 var manufatura = () => {
     createChat('63dee53e15d58dfce07eaaa496a479bac1376d31');
 }
@@ -7,35 +8,21 @@ var varejo = () => {
 var bemacash = () => {
     createChat('3e9ee6c5c4656822f19621bfbf8416972400751b');
 }
-
 var saude = () => {
-    removeDrozScript();
-    /*<![CDATA[*/window.zEmbed || function(e, t) { let n, o, d, i, s, a = [], r = document.createElement("iframe"); window.zEmbed = function () { a.push(arguments) }, window.zE = window.zE || window.zEmbed, r.src = "javascript:false", r.title = "", r.role = "presentation", (r.frameElement || r).style.cssText = "display: none", d = document.getElementsByTagName("script"), d = d[d.length - 1], d.parentNode.insertBefore(r, d), i = r.contentWindow, s = i.document; try { o = s; } catch (c) { n = document.domain, r.src = `javascript:var d=document.open();d.domain="${n}";void(0);`, o = s } o.open()._l = function () { let o = this.createElement("script"); n && (this.domain = n), o.id = "js-iframe-async", o.src = e, this.t = +new Date, this.zendeskHost = t, this.zEQueue = a, this.body.appendChild(o) }, o.write("<body onload=\"document._l();\">"), o.close() } ("https://assets.zendesk.com/embeddable_framework/main.js", "totvssuporte.zendesk.com");/*]]>*/
-    !!zE && zE(() => {
-            $zopim(function () {
-                $zopim.livechat.clearAll();
-                $zopim.livechat.hideAll();
-                $zopim.livechat.setLanguage("pt_BR");
-                $zopim.livechat.departments.filter(190811);
-                $zopim.livechat.departments.setVisitorDepartment(190811);
-                $zopim.livechat.addTags("chat_fly01_saude","chat_origem_fly01");
-                $zopim.livechat.theme.reload();
-                });
-        zE.show();
-    });
+    createChat('ffec1a060b7442f1379a9ea90662824a51ca7a10');
 }
 
-var openDroz = () => {
-    $("#drz_btn_open").length == 0 ?
-        setTimeout(openDroz, 300) :
-        $("#drz_btn_open").click();
-        
-    var intervalId = setInterval(function () {
-        if (typeof $zopim !== "undefined" && typeof $zopim.livechat !== "undefined") {
-            $zopim.livechat.say = function () { }
-            clearInterval(intervalId);
-        }
-    }, 1500);
+var createChat = key => {
+    if (window.zE !== undefined) zE.hide();
+    removeDrozScript();
+    (function (d, r, o, z, a, k, t) {
+        a = d.getElementsByTagName('head')[0];
+        k = d.createElement('script'); k.async = 1;
+        t = (Math.floor(Math.random() * 1000000) + 1);
+        k.src = r + '/' + o + '/droz.js?i=' + z + '&u=' + r + '&v=' + t;
+        a.appendChild(k);
+    })(document, 'https://chat-app.meudroz.com', 'v1', key);
+    openDroz();
 }
 
 var removeDrozScript = () => {
@@ -60,15 +47,42 @@ var removeDrozScript = () => {
     }
 }
 
-var createChat = key => {
-    if (window.zE !== undefined) zE.hide();
-    removeDrozScript();
-    (function (d, r, o, z, a, k, t) {
-        a = d.getElementsByTagName('head')[0];
-        k = d.createElement('script'); k.async = 1;
-        t = (Math.floor(Math.random() * 1000000) + 1);
-        k.src = r + '/' + o + '/droz.js?i=' + z + '&u=' + r + '&v=' + t;
-        a.appendChild(k);
-    })(document, 'https://chat-app.meudroz.com', 'v1', key);
-    openDroz();
+var openDroz = () => {
+    $("#drz_btn_open").length == 0 ?
+        setTimeout(openDroz, 300) :
+        $("#drz_btn_open").click();
+        
+    var intervalId = setInterval(function () {
+        if (typeof $zopim !== "undefined" && typeof $zopim.livechat !== "undefined") {
+            $zopim.livechat.say = function () { }
+            clearInterval(intervalId);
+        }
+    }, 1500);
+}
+
+var validaParam = () => {
+    var paramsURL = window.location.search;
+    var items = paramsURL.split("=");
+
+    var parametros = {
+        'id' : items[0],
+        'valor' : items[1]
+        }
+    console.log(parametros.id);
+    if (parametros.id == '?page') {
+        switch (parametros.valor) {
+            case 'manufatura':
+                manufatura();
+                break;
+            case 'saude':
+                saude();
+                break;
+            case 'varejo':
+                varejo();
+                break;
+            case 'bemacash':
+                bemacash();
+                break;
+        }
+    }
 }
